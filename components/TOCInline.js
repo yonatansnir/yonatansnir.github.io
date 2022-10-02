33714f8d06082ad24ea7a168e7eac85a08e45dff
Yonatan Snir <yonatan941@gmail.com>
2022-10-02 21:03:55 +0300

    add changes

--- components/TOCInline.js
+++ components/TOCInline.js
@@ -21,44 +21,51 @@
  *
  */
 const TOCInline = ({
-  toc,
-  indentDepth = 3,
-  fromHeading = 1,
-  toHeading = 6,
-  asDisclosure = false,
-  exclude = '',
+	toc,
+	indentDepth = 3,
+	fromHeading = 1,
+	toHeading = 6,
+	asDisclosure = false,
+	exclude = '',
 }) => {
-  const re = Array.isArray(exclude)
-    ? new RegExp('^(' + exclude.join('|') + ')$', 'i')
-    : new RegExp('^(' + exclude + ')$', 'i')
+	const re = Array.isArray(exclude)
+		? new RegExp('^(' + exclude.join('|') + ')$', 'i')
+		: new RegExp('^(' + exclude + ')$', 'i');
 
-  const filteredToc = toc.filter(
-    (heading) =>
-      heading.depth >= fromHeading && heading.depth <= toHeading && !re.test(heading.value)
-  )
+	const filteredToc = toc.filter(
+		(heading) =>
+			heading.depth >= fromHeading &&
+			heading.depth <= toHeading &&
+			!re.test(heading.value)
+	);
 
-  const tocList = (
-    <ul>
-      {filteredToc.map((heading) => (
-        <li key={heading.value} className={`${heading.depth >= indentDepth && 'ml-6'}`}>
-          <a href={heading.url}>{heading.value}</a>
-        </li>
-      ))}
-    </ul>
-  )
+	const tocList = (
+		<ul>
+			{filteredToc.map((heading) => (
+				<li
+					key={heading.value}
+					className={`${heading.depth >= indentDepth && 'ml-6'}`}
+				>
+					<a href={heading.url}>{heading.value}</a>
+				</li>
+			))}
+		</ul>
+	);
 
-  return (
-    <>
-      {asDisclosure ? (
-        <details open>
-          <summary className="ml-6 pt-2 pb-2 text-xl font-bold">Table of Contents</summary>
-          <div className="ml-6">{tocList}</div>
-        </details>
-      ) : (
-        tocList
-      )}
-    </>
-  )
-}
+	return (
+		<>
+			{asDisclosure ? (
+				<details open>
+					<summary className="ml-6 pt-2 pb-2 text-xl font-bold">
+						Table of Contents
+					</summary>
+					<div className="ml-6">{tocList}</div>
+				</details>
+			) : (
+				tocList
+			)}
+		</>
+	);
+};
 
-export default TOCInline
+export default TOCInline;
